da816c4dc0adf7d1c7d55a82af5afa8e0f3f5151
amarsadiku1 <amarsadiku79@gmail.com>
2026-01-14 17:44:46 +0100

    lesson 9

---
+++ lesson 9/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> 
+    <from action=" ">
+    <input type="text" name="name" placeholder="Name"><br> 
+    <input type="text" name="surname" placeholder="surname"><br> 
+    <input type="email" name="email" placeholder="Email"><br> 
+   <button type="sumbit" name="sumbit">Add</button> 
+</from>
+
+</body>
+</html>
